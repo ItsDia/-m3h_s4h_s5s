0dff37b1b73b9fa4fa456397283583be91572f80
ItsDia <1711949109@qq.com>
2024-05-26 13:01:40 +0800

    add x,y attributes to services

--- Service/integrated_forward_backward_search_with_updated_backward_search.py
+++ Service/integrated_forward_backward_search_with_updated_backward_search.py
@@ -102,10 +102,12 @@ def backward_search(pg, initial_concepts, goal_concepts):
     service_mapping = {}
     current_concepts = set(goal_concepts)
     initial = set(initial_concepts)
-    
+
     level = len(pg.a_levels) - 1
+    x = 300  # Initial x value
 
     while level >= 0:
+        y = 0  # Reset y value for each level
         tmp_level_concepts = set()
         services = set()
         for cpt in current_concepts:
@@ -116,8 +118,9 @@ def backward_search(pg, initial_concepts, goal_concepts):
 
         for serv in services:
             service_name_with_level = f"{serv.get_name()}-l{level}"
-            results["service"].append({"name": service_name_with_level})
+            results["service"].append({"name": service_name_with_level, "x": x, "y": y})
             service_mapping[(serv, level)] = service_name_with_level
+            y += 100  # Increase y value for each service in the same level
 
         current_concepts.clear()
         current_concepts.update(tmp_level_concepts)
@@ -125,6 +128,7 @@ def backward_search(pg, initial_concepts, goal_concepts):
         results["service"] = list({v['name']:v for v in results["service"]}.values())  # Remove duplicates
 
         level -= 1
+        x += 100  # Increase x value for each level
 
     # Create links
     for (service1, level1), name1 in service_mapping.items():
--- Service/models.py
+++ Service/models.py
@@ -1,15 +1,29 @@
-from django.db import models
+class Service:
+    def __init__(self, name, cost, input_concepts, output_concepts, x=300, y=0):
+        self.name = name
+        self.cost = cost
+        self.input_concepts = input_concepts
+        self.output_concepts = output_concepts
+        self.x = x
+        self.y = y
 
-# Create your models here.
+    def get_name(self):
+        return self.name
 
-#python manage.py makemigrations Service
-# python manage.py migrate
+    def get_cost(self):
+        return self.cost
 
-class Service(models.Model):
-    name = models.CharField(max_length=320)
-    cost = models.IntegerField()
-    input_concepts = models.TextField()
-    output_concepts = models.TextField()
+    def get_input_concept_set(self):
+        return self.input_concepts
 
+    def get_output_concept_set(self):
+        return self.output_concepts
 
+    def get_x(self):
+        return self.x
 
+    def get_y(self):
+        return self.y
+
+    def __str__(self):
+        return f"Service(name={self.name}, cost={self.cost}, input_concepts={self.input_concepts}, output_concepts={self.output_concepts}, x={self.x}, y={self.y})"
